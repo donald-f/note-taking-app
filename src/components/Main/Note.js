f66d63720da42450bbd529531160f6a3c66c9fc5
donald-f <donald.flobeck@gmail.com>
2022-12-06 16:45:13 -0500

    added a hook for local storage

--- src/components/Main/Note.js
+++ src/components/Main/Note.js
@@ -1,13 +1,27 @@
 import React from "react";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
 const Note = (props) => {
-  const noteParagraphs = props.paragraphs.map((para) => {
-    return <p className="body-text">{para}</p>;
+  const [blah, setBlah] = useLocalStorage("blah");
+
+  // need to NOT have key={i}
+  const noteParagraphs = props.paragraphs.map((para, i) => {
+    return (
+      <p className="body-text" key={i}>
+        {para}
+      </p>
+    );
   });
+
   return (
     <article>
       <h1 className="header-text">My Special Note</h1>
       {noteParagraphs}
+      <input
+        type="text"
+        value={blah}
+        onChange={(e) => setBlah(e.target.value)}
+      ></input>
     </article>
   );
 };
