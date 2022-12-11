c0978b0de30fc5fad80d1031bc2cad5438f6a373
donald-f <donald.flobeck@gmail.com>
2022-12-10 23:05:58 -0500

    Commented out dummy data

--- src/store/NotesProvider.js
+++ src/store/NotesProvider.js
@@ -5,20 +5,20 @@ import useLocalStorage from "../hooks/useLocalStorage";
 
 const NotesProvider = (props) => {
   const [notes, setNotes] = useLocalStorage("notes", [
-    {
-      id: "n1",
-      title: "My Special Note",
-      body: `You might remember the Dell computer commercials in which a youth reports this exciting news to his friends that they are about to get their new computer by telling them, “Dude, you’re getting a Dell!” It was a cute series but it reflects the excitement young people get about anything new, particularly if it’s a new machine.\nSo when its time to finally get your children that very first telescope, you want to make sure it’s just the right thing. There are a number of reasons you should put some serious thought into just what this beginner telescope should look like. Perhaps this will be your children’s first experience with a real telescope. They may have a healthy and thriving love of astronomy from your family trips to the country to watch a meteor shower or just to gaze at the stars. And you may have piqued their interest showing them how to enhance the experience with binoculars or even letting them play with your telescope.`,
-      active: true,
-      lastUpdated: "1:37PM 7/26/19",
-    },
-    {
-      id: "n2",
-      title: "My Other Note",
-      body: `I haven't told anyone about this note yet. \n\n\nLet's keep it between us`,
-      active: false,
-      lastUpdated: "12:10PM 12/10/22",
-    },
+    //  {
+    //    id: "n1",
+    //    title: "My Special Note",
+    //    body: `You might remember the Dell computer commercials in which a youth reports this exciting news to his friends that they are about to get their new computer by telling them, “Dude, you’re getting a Dell!” It was a cute series but it reflects the excitement young people get about anything new, particularly if it’s a new machine.\nSo when its time to finally get your children that very first telescope, you want to make sure it’s just the right thing. There are a number of reasons you should put some serious thought into just what this beginner telescope should look like. Perhaps this will be your children’s first experience with a real telescope. They may have a healthy and thriving love of astronomy from your family trips to the country to watch a meteor shower or just to gaze at the stars. And you may have piqued their interest showing them how to enhance the experience with binoculars or even letting them play with your telescope.`,
+    //    active: true,
+    //    lastUpdated: "1:37PM 7/26/19",
+    //  },
+    //  {
+    //    id: "n2",
+    //    title: "My Other Note",
+    //    body: `I haven't told anyone about this note yet. \n\n\nLet's keep it between us`,
+    //    active: false,
+    //    lastUpdated: "12:10PM 12/10/22",
+    //  },
   ]);
   const addNote = useCallback(
     (note) => {
